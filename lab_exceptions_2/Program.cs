14a14a9ee0bbdead057d3b293e923f6a50f7e18a
Roman <manko.roman1123@vu.cdu.edu.ua>
2024-11-20 21:35:31 +0200

    getfiles+regex

---
+++ lab_exceptions_2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Drawing;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        //список файлів у поточній папці
+        string currentDirectory = Directory.GetCurrentDirectory();
+        string[] files = Directory.GetFiles(currentDirectory);
+
+        //регулярний вираз перевірки графічних розширень
+        Regex imageExtensions = new Regex(@"(bmp|gif|tiff?|jpe?g|png)$", RegexOptions.IgnoreCase);
+
+        
+    }
+}
+
